8d4234981b6dac33a73905fa07e6cb099959b569
urvashi2004 <urvashi214btcse23@igdtuw.ac.in>
2025-05-11 06:06:02 +0530

    Some css changes

--- src/app/project/[id]/page.js
+++ src/app/project/[id]/page.js
@@ -39,7 +39,7 @@ export default function ProjectPage() {
     <div className={styles.projectPage}>
       <div className={styles.textContainer}>
         <h1 className={styles.title}>{project.title}</h1>
-        <p className={styles.description}>It&apos;s a project description.</p>
+        <p className={styles.description}>I&apos;m a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.</p>
       </div>
       <Image
         src={project.image}
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,19 +5,18 @@ import { getAuth } from "firebase/auth";
 import { getFirestore, collection, addDoc } from "firebase/firestore";
 import { GoogleAuthProvider, signInWithPopup, signInAnonymously, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
-
-// Initialize Firebase
+// Ensure Firebase initialization only runs on the client side
 let app;
 if (typeof window !== "undefined") {
+  const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  };
+
   app = initializeApp(firebaseConfig);
   isSupported().then((supported) => {
     if (supported) {
@@ -26,7 +25,7 @@ if (typeof window !== "undefined") {
   });
 }
 
-// Initialize Firebase services
+// Export Firebase services only if initialized
 export const auth = app ? getAuth(app) : null;
 export const db = app ? getFirestore(app) : null;
 
